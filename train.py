ae7012f76a21365df171c995656f14e13ba0536f
yanhao-dl <yanhao2@mails.gdut.edu.cn>
2026-01-12 16:01:19 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,60 +1,41 @@
-import time
-from options.train_options import TrainOptions
-from data import create_dataset
-from models import create_model
-from util.visualizer import Visualizer
-import copy
+from __future__ import division
+import os,time,cv2,scipy.io
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as st
+import argparse
+import subprocess
+from scipy.misc import imread,imsave
+import utils.utils as utils
+from model.network import UNet as UNet
+from model.network import UNet_SE as UNet_SE
+import math
+from glob import glob
+from loss.losses import *
+import random
+
+seed = 2020#2019
+np.random.seed(seed)
+tf.set_random_seed(seed)
+random.seed(seed)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--loss", default="lp", help="choose the loss type")
+parser.add_argument("--is_test", default=0,type=int, help="choose the loss type")
+parser.add_argument("--model", default="pre-trained",help="path to folder containing the model")
+parser.add_argument("--debug", default=0, type=int, help="DEBUG or not")
+parser.add_argument("--use_gpu", default=1, type=int, help="DEBUG or not")
+parser.add_argument("--save_model_freq", default=10, type=int, help="frequency to save model")
+
+ARGS = parser.parse_args()
+DEBUG = ARGS.debug 
+save_model_freq = ARGS.save_model_freq 
+model=ARGS.model
+is_test = ARGS.is_test
 
 
-if __name__ == '__main__':
-
-    opt = TrainOptions().parse()   # get training options
-    dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
-
-    dataset_size = len(dataset)    # get the number of images in the dataset.
-    print('The number of training images = %d' % dataset_size)
-
-    model = create_model(opt)      # create a model given opt.model and other options
-    model.setup(opt)               # regular setup: load and print networks; create schedulers
-    visualizer = Visualizer(opt)   # create a visualizer that display/save images and plots
-    total_iters = 0                # the total number of training iterations
-
-    for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):    # outer loop for different epochs; we save the model by <epoch_count>, <epoch_count>+<save_latest_freq>
-        epoch_start_time = time.time()  # timer for entire epoch
-        iter_data_time = time.time()    # timer for data loading per iteration
-        epoch_iter = 0                  # the number of training iterations in current epoch, reset to 0 every epoch
-
-        for i, data in enumerate(dataset):  # inner loop within one epoch
-            iter_start_time = time.time()  # timer for computation per iteration
-            if total_iters % opt.print_freq == 0:
-                t_data = iter_start_time - iter_data_time
-            visualizer.reset()
-            total_iters += opt.batch_size
-            epoch_iter += opt.batch_size
-            model.set_input(data)         # unpack data from dataset and apply preprocessing
-            model.optimize_parameters()   # calculate loss functions, get gradients, update network weights
-
-            if total_iters % opt.display_freq == 0:   # display images on visdom and save images to a HTML file
-                save_result = total_iters % opt.update_html_freq == 0
-                model.compute_visuals()
-                visualizer.display_current_results(model.get_current_visuals(), total_iters, save_result)
-
-            if total_iters % opt.print_freq == 0:   # print training losses and save logging information to the disk
-                t_comp = (time.time() - iter_start_time) / opt.batch_size
-                visualizer.print_current_losses(epoch, epoch_iter, model.get_current_losses(), t_comp, t_data)
-                if opt.display_id > 0:
-                    visualizer.plot_current_losses(epoch, float(epoch_iter) / dataset_size, model.get_current_losses())
-            iter_data_time = time.time()
-
-            if total_iters % opt.save_latest_freq == 0:   # cache our latest model every <save_latest_freq> iterations
-                print('saving the latest model (epoch %d, total_iters %d)' % (epoch, total_iters))
-                save_suffix = 'iter_%d' % total_iters if opt.save_by_iter else 'latest'
-                model.save_networks(save_suffix)
-
-        if epoch % opt.save_epoch_freq == 0:              # cache our model every <save_epoch_freq> epochs
-            print('saving the model at the end of epoch %d, iters %d' % (epoch, total_iters))
-            model.save_networks('latest')
-            model.save_networks(epoch)
-
-        print('End of epoch %d / %d \t Time Taken: %d sec' % (epoch, opt.niter + opt.niter_decay, time.time() - epoch_start_time))
-        model.update_learning_rate()                     # update learning rates at the end of every epoch.
+def calculate_psnr(img1, img2):
+    # update learning rates at the end of every epoch.
+    pass
